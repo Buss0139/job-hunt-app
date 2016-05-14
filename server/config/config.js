3c8ef4cf83f85100ce1a6ef4d2f2980381ad785b
Jana <jana@creedinteractive.com>
2016-05-14 11:29:15 -0500

    comment out PostreSQL for now

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ var path = require('path'),
 
 module.exports = {
     development: {
-        db: 'postgres://xgnoosjanczkch:Xg6hI-ioy94Ug75xFy_EPc4gSN@ec2-54-243-249-144.compute-1.amazonaws.com:5432/dfsgqqa1pdqo93',
+        //db: 'postgres://xgnoosjanczkch:Xg6hI-ioy94Ug75xFy_EPc4gSN@ec2-54-243-249-144.compute-1.amazonaws.com:5432/dfsgqqa1pdqo93',
         root: rootPath,
         port: 27017,
         app: {
