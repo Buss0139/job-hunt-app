1b33546f4789d82f13a71157be2579cb1681d3c5
Amber Joy <amber.joy@gmail.com>
2016-05-14 10:16:51 -0500

    adds start script to package.json

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -66,10 +66,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     // Default task(s).
-    grunt.registerTask('default', ['copy', 'uglify', 'jshint', 'sass']);
+    grunt.registerTask('default', ['copy', 'uglify', 'sass']);
 
 };
